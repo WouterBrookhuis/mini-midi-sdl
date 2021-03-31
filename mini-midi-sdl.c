947e063f9919f56a362525faab013670bb1bb099
Wouter Brookhuis <wrdbrookhuis@gmail.com>
2021-03-31 23:42:08 +0200

    Parse tracks

--- mini-midi-sdl.c
+++ mini-midi-sdl.c
@@ -17,21 +17,65 @@ int main(int argc, char **argv)
     return -1;
   }
 
-  size_t bytesRead = fread(&fileBuffer[0], sizeof(uint8_t), sizeof(fileBuffer), midiFile);
+  size_t totalBytes = fread(&fileBuffer[0], sizeof(uint8_t), sizeof(fileBuffer), midiFile);
   fclose(midiFile);
 
-  printf("Read %lu bytes\n", bytesRead);
+  printf("Read %lu bytes\n", totalBytes);
 
   mm_header_t midiHeader;
-  if (!mm_read_header(&fileBuffer[0], bytesRead, &midiHeader))
+  size_t offset = 0;
+  size_t bytesRead = mm_read_header(&fileBuffer[0], totalBytes, &midiHeader);
+  if (0 == bytesRead)
   {
     printf("Failed to read header\n");
     return -2;
   }
 
+  offset += bytesRead;
+
   printf("Format: %u\n", midiHeader.format);
   printf("Tracks: %u\n", midiHeader.trackCount);
   printf("Time: 0x%04X\n", midiHeader.timeDivision);
+
+  for (uint16_t track = 0; track < midiHeader.trackCount; track++)
+  {
+    uint32_t trackSize;
+    bytesRead = mm_read_track_chunk(&fileBuffer[offset], totalBytes - offset, &trackSize);
+    if (0 == bytesRead)
+    {
+      printf("Error reading track chunk %u\n", track);
+      return -3;
+    }
+
+    printf("Track %u at offset %lu is %u bytes long\n", track, offset, trackSize);
+
+    offset += bytesRead;
+
+    while (trackSize > 0)
+    {
+      mm_event_t event;
+      bytesRead = mm_read_track_event(&fileBuffer[offset], totalBytes - offset, &event);
+      if (0 == bytesRead)
+      {
+        printf("Error reading event in track %u (file offset %lu)\n", track, offset);
+        for (size_t i = 0; i < 16; i++)
+          printf("%02X ", fileBuffer[offset + i]);
+        printf("\n");
+        return -3;
+      }
+
+      printf("Track event %X on channel %u\n", event.type, event.note.channel);
+
+      if (bytesRead > trackSize)
+      {
+        printf("Read event outside of track %u (file offset %lu)\n", track, offset);
+        return -4;
+      }
+
+      offset += bytesRead;
+      trackSize -= bytesRead;
+    }
+  }
 
   printf("Hello, MIDI!\n");
   return 0;
